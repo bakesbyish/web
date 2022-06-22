912d9784f6e4a38dd5b63aaf8c023c33fd0cc496
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-06-22 09:51:26 +0530

    Created the aspect ratio configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
   plugins: [require('@tailwindcss/aspect-ratio')],
 };
