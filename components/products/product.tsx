737a1340a83f40128ca4a9f857e5437d291aca4d
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-06-25 11:10:38 +0530

    Implemented the add to cart functionality

--- components/products/product.tsx
+++ components/products/product.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { HeartIcon } from '@heroicons/react/solid';
 import { RadioGroup } from '@headlessui/react';
-import { ISlugProduct } from '@interfaces/products';
+import { ICart, IProduct } from '@interfaces/products';
 import { classNames } from '@lib/utils';
 import Image from 'next/image';
 import { Color } from '@components/utils/color';
+import { MinusIcon, PlusIcon } from '@heroicons/react/solid';
+import { useCart } from 'react-use-cart';
 
-export const Product = (props: { product: ISlugProduct }) => {
+export const Product = (props: { product: IProduct }) => {
   const { product } = props;
+
+  const { addItem } = useCart();
 
   const [selectedVariant, setSelectedVariant] = useState(
     product.hasVariants ? product.productVariants[0] : null
@@ -18,15 +22,73 @@ export const Product = (props: { product: ISlugProduct }) => {
   const [selectedColor, setSelectedColor] = useState(
     product.hasColors ? product.productColors[0].color : null
   );
+  const [price, setPrice] = useState<number>(
+    selectedVariant?.price || product.price
+  );
+  const [qty, setQty] = useState<number>(1);
+
+  // Update the price when there are discounts for specific quantities
+  useEffect(() => {
+    if (selectedVariant) {
+      if (selectedVariant.hasDiscounts) {
+        setPrice(
+          qty >= (selectedVariant as any).discountedFrom
+            ? (selectedVariant.discountedPrice as number)
+            : (selectedVariant.price as number)
+        );
+      } else if (product.hasDiscounts) {
+        setPrice(
+          qty >= (product as any).discountedFrom
+            ? (product.discountedPrice as number)
+            : (selectedVariant.price as number)
+        );
+      }
+    } else if (product.hasDiscounts) {
+      setPrice(
+        qty >= (product as any).discountedFrom
+          ? (product.discountedPrice as number)
+          : (product.price as number)
+      );
+    }
+  }, [qty, selectedVariant, product]);
+
+  const handleQtyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(event.target.value);
+
+    if (value > 0) {
+      setQty(value);
+    }
+  };
+
+  // Add the product to the cart when use clicks the cart button
+  const handleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const selectedProduct = {
+      id: product.sku,
+			slug: product.slug,
+      url: selectedVariant?.url || product.url,
+      name: product.title,
+      color: selectedVariantColor || selectedColor,
+      size: selectedVariant?.name || null,
+      price: price,
+    } as ICart;
+
+    addItem(selectedProduct, qty);
+  };
 
   return (
     <div>
       <div className="pt-6">
         {/* Image gallery */}
         <div className="mt-6 max-w-2xl mx-auto px-3 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-1">
-          <div className="aspect-w-4 aspect-h-4 sm:rounded-lg sm:mx-auto sm:aspect-w-8 sm:aspect-h-4 sm:w-96 sm:h-96 lg:aspect-w-8 lg:aspect-h-2 lg:w-96 lg:h-96 justify-self-center">
+          <div
+            className={classNames(
+              'aspect-w-4 aspect-h-4 sm:rounded-lg sm:mx-auto sm:aspect-w-8 sm:aspect-h-4 sm:w-96 sm:h-96 lg:aspect-w-8 lg:aspect-h-2 lg:w-96 lg:h-96 justify-self-center'
+            )}
+          >
             <Image
-							priority={true}
+              priority={true}
               src={props.product.url || product.url}
               alt={product.title}
               layout="fill"
@@ -47,7 +109,7 @@ export const Product = (props: { product: ISlugProduct }) => {
           <div className="mt-4 lg:mt-0 lg:row-span-3">
             <h2 className="sr-only">Product information</h2>
             <p className="text-3xl text-gray-900 dark:text-white">
-              {selectedVariant?.price || product.price}
+              LKR {price}
             </p>
 
             {/* Hearts */}
@@ -63,7 +125,7 @@ export const Product = (props: { product: ISlugProduct }) => {
               </div>
             </div>
 
-            <form className="mt-10">
+            <form onSubmit={handleSubmit} className="mt-10">
               {/* Colors */}
               {selectedVariantColor && (
                 <Color
@@ -105,9 +167,10 @@ export const Product = (props: { product: ISlugProduct }) => {
                           value={variant}
                           className={({ active }) =>
                             classNames(
-                              'bg-white shadow-sm text-gray-900 cursor-pointer',
+                              'bg-white dark:bg-gray-700 dark:text-white shadow-sm text-gray-900 cursor-pointer',
                               active ? 'ring-2 ring-rose-300' : '',
-                              'group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'
+                              'group relative border rounded-md py-4 px-4 flex items-center justify-center text-sm font-medium uppercase',
+                              'hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-3'
                             )
                           }
                         >
@@ -157,9 +220,42 @@ export const Product = (props: { product: ISlugProduct }) => {
                 </div>
               ) : null}
 
+              <span className="sr-only">Choose the quantity</span>
+
+              <section
+                aria-hidden="true"
+                className="mt-8 flex items-center justify-center gap-2"
+              >
+                <PlusIcon
+                  type="button"
+                  onClick={() => {
+                    setQty(qty + 1);
+                  }}
+                  className="w-10 h-10 bg-slate-100 dark:bg-gray-700 py-2 rounded-lg"
+                />
+                <input
+                  type="number"
+                  min={1}
+                  value={qty || ''}
+                  onChange={handleQtyChange}
+                  prefix="g"
+                  className="text-center bg-slate-100 dark:bg-gray-700 py-2 w-32 rounded-lg"
+                />
+                {product.unit ? product.unit : null}
+                <MinusIcon
+                  onClick={() => {
+                    setQty(qty > 1 ? qty - 1 : 1);
+                  }}
+                  className="w-10 h-10 bg-slate-100 dark:bg-gray-700 py-2 rounded-lg"
+                />
+              </section>
+
               <button
                 type="submit"
-                className="mt-10 w-full bg-rose-300 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-black hover:bg-rose-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-400"
+                className={classNames(
+                  'mt-10 w-full bg-rose-300 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-black hover:bg-rose-400',
+                  'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-400'
+                )}
               >
                 Add to bag
               </button>
