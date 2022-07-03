0e254b7032cb6ea9d0c77be7412a6873e8578899
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-03 23:17:59 +0530

    Added realtime databse

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,10 +3,12 @@ import { FirebaseApp, getApp, getApps, initializeApp } from 'firebase/app';
 import { Auth, getAuth } from 'firebase/auth';
 import { FirebaseStorage, getStorage } from 'firebase/storage';
 import { Firestore, getFirestore } from 'firebase/firestore';
+import { getDatabase, Database } from 'firebase/database';
 
 const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -26,3 +28,6 @@ export const storage: FirebaseStorage = getStorage(app);
 
 // Initialize firestore
 export const db: Firestore = getFirestore(app);
+
+// Initialize realtime database
+export const realtime: Database = getDatabase(app);
