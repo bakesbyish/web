2369628aa7d7aa12d9d7e9b203f2d4c81e8adc7e
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-18 11:26:31 +0530

    Update the firebase firestore upon submit

--- components/profile/notifications.tsx
+++ components/profile/notifications.tsx
@@ -1,6 +1,33 @@
+import { Loader } from '@components/utils/loader';
+import { useProfileContext } from '@context/profile';
+import { database } from '@interfaces/firestore';
 import { classNames } from '@lib/utils';
+import { db } from 'config/firebase';
+import { doc, updateDoc } from 'firebase/firestore';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export const Notifications = () => {
+  const { user } = useProfileContext();
+
+  const [value, setValue] = useState<boolean | undefined>(user?.notifications);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const userRef = doc(db, database.users, user!.uid);
+    const ref = database.collections.users;
+
+    if (value !== user?.notifications) {
+      await updateDoc(userRef, {
+        [ref.notifications]: value,
+      });
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="mt-10 sm:mt-0">
       <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -33,6 +60,9 @@ export const Notifications = () => {
                           id="offers"
                           name="offers"
                           type="checkbox"
+                          defaultChecked={value}
+                          onChange={() => setValue(!value)}
+                          disabled={loading}
                           className="focus:ring-rose-400 h-4 w-4 text-rose-600 border-gray-300 rounded"
                         />
                       </div>
@@ -44,8 +74,8 @@ export const Notifications = () => {
                           Offers
                         </label>
                         <p className="text-gray-500 dark:text-white/80">
-                          Get notified when a candidate accepts or rejects an
-                          offer.
+                          Get notified about new promotions, custom offers and
+                          more via email
                         </p>
                       </div>
                     </div>
@@ -55,12 +85,17 @@ export const Notifications = () => {
               <div className="px-4 py-3 dark:border-t dark:border-slate-600 bg-gray-50 dark:bg-gray-900 text-right sm:px-6">
                 <button
                   type="submit"
+                  disabled={loading}
                   className={classNames(
                     'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-rose-400',
                     'hover:bg-rose-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-400'
                   )}
                 >
-                  Save
+                  {loading ? (
+                    <Loader width={5} height={5} />
+                  ) : (
+                    <span>Save</span>
+                  )}
                 </button>
               </div>
             </div>
