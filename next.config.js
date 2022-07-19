f7bc789303841bfbb5ce0a46620e7ad56a0dd091
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-19 20:38:18 +0530

    Added sanity as a allowed URL

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: [
-      'media.graphassets.com',
-      'loremflickr.com',
-      'ik.imagekit.io',
-      'lh3.googleusercontent.com',
-      'avatars.dicebear.com',
-       'firebasestorage.googleapis.com'
+      "media.graphassets.com",
+      "loremflickr.com",
+      "ik.imagekit.io",
+      "lh3.googleusercontent.com",
+      "avatars.dicebear.com",
+      "firebasestorage.googleapis.com",
+      "cdn.sanity.io",
     ],
   },
 };
