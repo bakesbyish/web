2b91c626775b5d02b59dc1b2963cf4509fc8c9da
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-19 20:38:57 +0530

    Replaced graphcms with sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ import { Hero } from '@components/utils/hero';
 import { Loader } from '@components/utils/loader';
 import { Trending } from '@components/utils/trending';
 import { ICollectionCard, ICollectionProduct } from '@interfaces/products';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { client } from 'config/apollo';
+import { sanity, urlFor } from 'config/sanity';
+import { collection } from 'firebase/firestore';
 import { GetStaticProps } from 'next';
 import dynamic from 'next/dynamic';
 import { ReactElement } from 'react';
@@ -15,19 +18,21 @@ import { useInView } from 'react-cool-inview';
 // Lazy loaded components
 const Newsletter = dynamic<any>(
   () => import('@components/utils/newsletter').then((mod) => mod.Newsletter),
-  { loading: () => (
-		<section className="mb-5 mt-5">
-			<Loader />
-		</section>
-	)}
+  {
+    loading: () => (
+      <section className="mb-5 mt-5">
+        <Loader />
+      </section>
+    ),
+  }
 );
 
 const GeoLocation = dynamic<any>(() => import('@components/utils/map'), {
   loading: () => (
-		<section className="mb-5 mt-5">
-			<Loader />
-		</section>
-	),
+    <section className="mb-5 mt-5">
+      <Loader />
+    </section>
+  ),
 });
 
 export default function Home(props: {
@@ -37,7 +42,7 @@ export default function Home(props: {
   const { trendingProducts, collections } = props;
 
   const { observe, inView } = useInView({
-		rootMargin: "100px 0px",
+    rootMargin: '100px 0px',
     onEnter: ({ unobserve }) => unobserve(),
   });
 
@@ -50,12 +55,12 @@ export default function Home(props: {
         <Trending trendingProducts={trendingProducts} />
         <Collections collections={collections} />
         <div ref={observe} className="w-full">
-					{inView ? (
-						<>
-							<Newsletter />
-							<GeoLocation />
-						</>
-					) : null}
+          {inView ? (
+            <>
+              <Newsletter />
+              <GeoLocation />
+            </>
+          ) : null}
         </div>
       </main>
     </div>
@@ -68,42 +73,75 @@ Home.getLayout = function getLayout(page: ReactElement) {
 
 export const getStaticProps: GetStaticProps = async () => {
   // Get data for trending products
-  const trendingProducts = await client.query({
-    query: gql`
-      query {
-        products(where: { trending: true }, first: 4, orderBy: createdAt_DESC) {
-          title
-          slug
-          sku
-          image {
-            url
-          }
-          price
-        }
-      }
-    `,
+  const trendingProductsData = (await sanity.fetch(
+    `*[_type == "products" && trending == true] | order(_createdAt desc)[0..3] {
+				title,
+				slug {
+					current
+				},
+				sku,
+				image,
+				price
+			}`
+  )) as {
+    title: string;
+    slug: {
+      current: string;
+    };
+    sku: number;
+    image: SanityImageSource;
+    price: number;
+  }[];
+
+  const trendingProducts: ICollectionProduct[] = [];
+
+  trendingProductsData.map((product) => {
+    trendingProducts.push({
+      title: product.title,
+      slug: product.slug.current,
+      sku: product.sku,
+      image: {
+        url: urlFor(product.image).url(),
+      },
+    });
   });
 
   // Get data for collections
-  const collections = await client.query({
-    query: gql`
-      query {
-        catergories(orderBy: createdAt_DESC, first: 3) {
-          catergory
-          catergorySlug
-          image {
-            url
-          }
-          catergoryDescription
-        }
-      }
-    `,
+  const collectionData = (await sanity.fetch(
+    `*[_type == "categories"] | order(_createdAt desc)[0..2]{
+				title,
+				slug {
+					current
+				},
+				image,
+				description
+			}`
+  )) as {
+    title: string;
+    slug: {
+      current: string;
+    };
+    image: SanityImageSource;
+    description: string;
+  }[];
+
+  const collections: ICollectionCard[] = [];
+
+  collectionData.map((collection) => {
+    collections.push({
+      catergory: collection.title,
+      catergorySlug: collection.slug.current,
+      image: {
+        url: urlFor(collection.image).url(),
+      },
+      catergoryDescription: collection.description,
+    });
   });
 
   return {
     props: {
-      trendingProducts: trendingProducts.data?.products,
-      collections: collections.data?.catergories,
+      trendingProducts,
+      collections,
     },
     revalidate: 10,
   };
