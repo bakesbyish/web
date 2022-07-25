e6f1afab8f0aad277d0a4a9eaac1538014800d33
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-25 13:51:58 +0530

    Testing with google json ldt

--- components/seo/shop.tsx
+++ components/seo/shop.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 
 interface IShopSeo {
   title: string;
@@ -14,28 +13,35 @@ interface IShopSeo {
 export const ShopSeo = (props: IShopSeo) => {
   const { title, description, url, image, price, slug, collection } = props;
   return (
-    <>
-      <Head>
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
-        <meta property="og:image" content={image} />
-        <meta property="product:brand" content="Facebook" />
-        <meta property="product:availability" content="in stock" />
-        <meta property="product:condition" content="new" />
-        <meta property="product:price:amount" content={price.toString()} />
-        <meta property="product:price:currency" content="LKR" />
-        <meta property="product:retailer_item_id" content={slug} />
+    <Head>
+      {/* Facebook */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="product:brand" content="Facebook" />
+      <meta property="product:availability" content="in stock" />
+      <meta property="product:condition" content="new" />
+      <meta property="product:price:amount" content={price.toString()} />
+      <meta property="product:price:currency" content="LKR" />
+      <meta property="product:retailer_item_id" content={slug} />
 
-        {collection ? (
-          <meta property="product:item_group_id" content={collection} />
-        ) : null}
-      </Head>
-      <Script
-        id="google-product-content"
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@bakesbyish" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+
+      {collection ? (
+        <meta property="product:item_group_id" content={collection} />
+      ) : null}
+
+      <script
+        type="application/ld+json"
         dangerouslySetInnerHTML={{
           __html: `
-						{
+					{
 						"@context": "https://schema.org/",
 						"@type": "Product",
 						"name": "Executive Anvil",
@@ -80,7 +86,8 @@ export const ShopSeo = (props: IShopSeo) => {
 					}
 					`,
         }}
+        key="google-product-jsonld"
       />
-    </>
+    </Head>
   );
 };
