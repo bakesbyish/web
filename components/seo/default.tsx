70290348e1e6a5c0c2af98f7b3bfc7fe8eae389a
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-26 14:30:46 +0530

    Using next script

--- components/seo/default.tsx
+++ components/seo/default.tsx
@@ -2,6 +2,7 @@ import { IPaths } from '@interfaces/seo';
 import { getBreadCrumbs } from '@lib/seo';
 import { URL } from 'config';
 import Head from 'next/head';
+import Script from 'next/script';
 
 interface IDefaultSeo {
   title?: string;
@@ -23,43 +24,46 @@ export const DefaultSeo = (props: IDefaultSeo) => {
   const robots = props.disableRobots ? true : false;
 
   return (
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
 
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="canonical" href={url} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="canonical" href={url} />
 
-      {/* OpenGraph metatags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:url" content={url} />
-      <meta property="og:image" content={image} />
-      <meta property="og:site_name" content={'Bakes By Ish'} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+        {/* OpenGraph metatags */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={image} />
+        <meta property="og:site_name" content={'Bakes By Ish'} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
 
-      {/* Twitter metatags */}
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@bakesbyish" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+        {/* Twitter metatags */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@bakesbyish" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
 
-      {/* Facebook metatags */}
-      <meta property="fb:admins" content="221988029638214" />
+        {/* Facebook metatags */}
+        <meta property="fb:admins" content="221988029638214" />
 
-      {/* Hide the page from robots */}
-      {robots ? <meta name="robots" content="noindex" /> : null}
+        {/* Hide the page from robots */}
+        {robots ? <meta name="robots" content="noindex" /> : null}
+      </Head>
 
       {/* Breadcrumbs for Google */}
       {paths ? (
-        <script
+        <Script
+          id="google-bread-crumbs"
           type="application/ld+json"
           dangerouslySetInnerHTML={getBreadCrumbs(paths, URL)}
         />
       ) : null}
-    </Head>
+    </>
   );
 };
