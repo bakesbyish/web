dde30f5b77384a5c1f7ae163974c312bbf97cf8b
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-26 10:56:47 +0530

    Installed fb pixel to fire on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
-import { ReactElement, ReactNode, useState } from 'react';
+import { ReactElement, ReactNode, useEffect, useState } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { CartProvider } from 'react-use-cart';
 import { BakesbyIshContext } from '@context/context';
 import { useUserData } from '@hooks/use-user-data';
 import { ProgressBar } from '@components/utils/progress';
 import { Toast } from '@components/utils/toast';
+import * as fbq from '@lib/fbpixel';
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -20,27 +23,60 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const [cartOpen, setCartOpen] = useState<boolean>(false);
   const { user, validating, mutate } = useUserData();
+  const router = useRouter();
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
+  useEffect(() => {
+    fbq.pageview();
+
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <BakesbyIshContext.Provider
-      value={{
-        user,
-        validating,
-        cartOpen,
-        setCartOpen,
-        mutate,
-      }}
-    >
-      <ProgressBar />
-      <CartProvider>
-        <ThemeProvider enableSystem={true} attribute={'class'}>
-          <Toast />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-      </CartProvider>
-    </BakesbyIshContext.Provider>
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+      <BakesbyIshContext.Provider
+        value={{
+          user,
+          validating,
+          cartOpen,
+          setCartOpen,
+          mutate,
+        }}
+      >
+        <ProgressBar />
+        <CartProvider>
+          <ThemeProvider enableSystem={true} attribute={'class'}>
+            <Toast />
+            {getLayout(<Component {...pageProps} />)}
+          </ThemeProvider>
+        </CartProvider>
+      </BakesbyIshContext.Provider>
+    </>
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,20 @@
+import { FB_PIXEL_ID } from '@lib/fbpixel';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
+        </Head>
 
         <body className="scrollbar bg-white text-black dark:bg-gray-800 dark:text-white">
           <Main />
