bf4e2d503562e3c69269e398abc8bc0df2177b14
Vinuka Kodituwakku <vinuka.kodituwakku@gmail.com>
2022-07-27 00:22:59 +0530

    Extracted the external script to a component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,9 @@ import { useUserData } from '@hooks/use-user-data';
 import { ProgressBar } from '@components/utils/progress';
 import { Toast } from '@components/utils/toast';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
 import * as fbq from '@lib/fbpixel';
 import '../styles/globals.css';
+import { ExternalScripts } from '@components/utils/scripts';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -42,46 +42,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
-      <Script
-        id="fb-pixel"
-        strategy="worker"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
-          `,
-        }}
-      />
-
-      <div id="fb-root"></div>
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script id="fb-messenger" strategy="lazyOnload">
-        {`
-					var chatbox = document.getElementById('fb-customer-chat');
-					chatbox.setAttribute("page_id", "112675403902811");
-					chatbox.setAttribute("attribution", "biz_inbox");
-					window.fbAsyncInit = function() {
-						FB.init({
-							xfbml            : true,
-							version          : 'v14.0'
-						});
-					};
-					(function(d, s, id) {
-						var js, fjs = d.getElementsByTagName(s)[0];
-						if (d.getElementById(id)) return;
-						js = d.createElement(s); js.id = id;
-						js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-						fjs.parentNode.insertBefore(js, fjs);
-					}(document, 'script', 'facebook-jssdk'));
-				`}
-      </Script>
+      <ExternalScripts />
 
       <BakesbyIshContext.Provider
         value={{
